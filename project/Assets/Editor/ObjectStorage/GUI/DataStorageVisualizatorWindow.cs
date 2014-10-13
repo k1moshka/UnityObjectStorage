857f2debee7c84088f772388a8e03bb883efb892
k1moshka <364844@gmail.com>
2014-10-13 23:33:02 +0700

    импортирование проекта

---
+++ project/Assets/Editor/ObjectStorage/GUI/DataStorageVisualizatorWindow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// Окно инициализатора сторэйджа для конкретного типа объектов
+/// </summary>
+public class DataStorageVisualizatorWindow : EditorWindow
+{
+    [MenuItem("Assets/Manage Storage...")]
+    [ContextMenu("Manage Storage...")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(DataStorageVisualizatorWindow));
+    }
+
+    public void OnGUI()
+    {
+
+    }
+}
---
+++ project/Assets/Editor/ObjectStorage/GUI/ObjectSchemeWindow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System;
+
+/// <summary>
+/// Окно для создания схемы объекта, его полей и т.д.
+/// </summary>
+public class ObjectSchemeWindow : EditorWindow 
+{
+    [MenuItem("Assets/Create new object storage...")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(ObjectSchemeWindow));
+    }
+
+    // рисование интерфейса
+    private string DataID = "";
+    public void OnGUI()
+    {
+        GUILayout.BeginVertical();
+        GUILayout.Label("Change object scheme");
+        DataID = EditorGUILayout.TextField("Data Id:", DataID);
+        GUILayout.Label("Data fields");
+        GUILayout.EndVertical();
+    }
+}
