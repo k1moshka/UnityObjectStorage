85aca9b8a2ca7638a621820fd2489eccaa9b026a
k1moshka <364844@gmail.com>
2014-10-16 21:57:29 +0700

    Обновлен интерфейс, добавлена визуализация  для полей и сохранение текущей схемы

--- project/Assets/Editor/ObjectStorage/GUI/ObjectSchemeWindow.cs
+++ project/Assets/Editor/ObjectStorage/GUI/ObjectSchemeWindow.cs
@@ -2,6 +2,7 @@
 using UnityEditor;
 using System.Collections.Generic;
 using System;
+using UnityStaticData;
 
 /// <summary>
 /// Окно для создания схемы объекта, его полей и т.д.
@@ -9,8 +10,12 @@ using System;
 public class ObjectSchemeWindow : EditorWindow 
 {
     // рисование интерфейса
-     // TODO: imlement
+     // TODO: implement
     private string DataID = "";
+    private int fieldsCount;
+
+    private List<string> fieldNames;
+    private List<string> fieldTypes;
 
     public void OnGUI()
     {
@@ -18,26 +23,61 @@ public class ObjectSchemeWindow : EditorWindow
         GUILayout.Label("Change object scheme");
         DataID = EditorGUILayout.TextField("Data Id:", DataID);
         GUILayout.Label("Data fields");
+
+        if (GUILayout.Button("Add scheme field"))
+        {
+            fieldsCount++;
+            fieldNames.Add(string.Empty);
+            fieldTypes.Add(string.Empty);
+        }
+        for (int i = 0; i < fieldsCount; i++)
+        {
+            EditorGUILayout.Separator();
+            fieldNames[i] = EditorGUILayout.TextField("Field Name:", fieldNames[i]);
+            fieldTypes[i] = EditorGUILayout.TextField("Field Type:", fieldTypes[i]);
+        }
+        EditorGUILayout.Separator();
+        EditorGUILayout.Separator();
+        EditorGUILayout.Separator();
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Save")) SaveScheme();
+        GUILayout.EndHorizontal();
+
         GUILayout.EndVertical();
     }
 
     [MenuItem("Assets/Create new object storage...")]
     public static void ShowWindow()
     {
-        EditorWindow.GetWindow(typeof(ObjectSchemeWindow));
+        var window = EditorWindow.GetWindow(typeof(ObjectSchemeWindow)) as ObjectSchemeWindow;
+        window.LoadScheme("sample scheme");
+    }
+
+    public void OnDestroy()
+    {
+        SaveScheme();
     }
 
 //------------- public interface
+    private string schemeName;
     private DataScheme dataScheme;
     public DataScheme DataScheme { get { return DataScheme; } }
 
     public void LoadScheme(string schemeName)
     {
+        dataScheme = SchemeStorage.GetScheme(schemeName);
+        fieldsCount = dataScheme.Fields.Count;
 
+        fieldNames = new List<string>(dataScheme.Fields.Keys);
+        fieldTypes = new List<string>(dataScheme.Fields.Values);
+
+        this.schemeName = schemeName;
     }
 
     public void SaveScheme()
     {
-        // TODO: save at temp unity storage
+        SchemeStorage.SaveScheme(schemeName, dataScheme);
+        SchemeStorage.SaveAtProject();
     }
 }
