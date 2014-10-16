d600c62dbc827d8b66de51f72f8f1676c051fc1a
k1moshka <364844@gmail.com>
2014-10-16 21:56:32 +0700

    Дописан полностью класс хранилище схем данных, реализовано, сохранение, загрузка схем данных

--- project/Assets/Editor/ObjectStorage/SchemeStorage.cs
+++ project/Assets/Editor/ObjectStorage/SchemeStorage.cs
@@ -1,32 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 
-[Serializable]
-public class SchemeStorage
+namespace UnityStaticData
 {
-    public Dictionary<string, DataScheme> schemes;
-
-    private SchemeStorage()
+    /// <summary>
+    /// Независимое хранилище схем данных используемое для хранения в постоянном формате схем данных
+    /// </summary>
+    [Serializable]
+    public class SchemeStorage
     {
-        schemes = new Dictionary<string, DataScheme>();
-    }
+        /// <summary>
+        /// Все схемы проекта
+        /// </summary>
+        public Dictionary<string, DataScheme> schemes;
 
-    private readonly static SchemeStorage _instance;
+        private SchemeStorage()
+        {
+            schemes = new Dictionary<string, DataScheme>();
+        }
 
-    static SchemeStorage()
-    {
-        _instance = new SchemeStorage();
-    }
+        private readonly static SchemeStorage _instance;
 
-    public static DataScheme GetScheme(string schemeName)
-    {
-        if (!_instance.schemes.ContainsKey(schemeName) || _instance.schemes[schemeName] == null)
-            _instance.schemes[schemeName] = new DataScheme();
-        return _instance.schemes[schemeName];
-    }
-
-    public static void SaveScheme(string schemeName, DataScheme scheme)
-    {
-        _instance.schemes[schemeName] = scheme;
+        static SchemeStorage()
+        {
+            _instance = new SchemeStorage();
+        }
+        /// <summary>
+        /// Поучить схему по имени, если таковой нет, то создается новая с таким именем
+        /// </summary>
+        /// <param name="schemeName">Имя схемы</param>
+        /// <returns></returns>
+        public static DataScheme GetScheme(string schemeName)
+        {
+            if (!_instance.schemes.ContainsKey(schemeName) || _instance.schemes[schemeName] == null)
+                _instance.schemes[schemeName] = new DataScheme();
+            return _instance.schemes[schemeName];
+        }
+        /// <summary>
+        /// Сохранение схемы, в объекте
+        /// </summary>
+        /// <param name="schemeName"></param>
+        /// <param name="scheme"></param>
+        public static void SaveScheme(string schemeName, DataScheme scheme)
+        {
+            _instance.schemes[schemeName] = scheme;
+        }
+        /// <summary>
+        /// Сохранение всех схем в папке проекта
+        /// </summary>
+        public static void SaveAtProject()
+        {
+            Serializator.SaveTo<SchemeStorage>(
+                Settings.GetPathToSaveData("schemesStorage.bin"), 
+                _instance);
+        }
     }
 }
