136602a274c7254c3b6f99cc8763b26da6b8164c
k1moshka <364844@gmail.com>
2014-10-16 21:54:32 +0700

    Добавление настроек для плагина

---
+++ project/Assets/Editor/ObjectStorage/GUI/SettingsWindow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System;
+
+/// <summary>
+/// Окно для создания схемы объекта, его полей и т.д.
+/// </summary>
+public class SettingsWindow : EditorWindow
+{ 
+    public void OnGUI()
+    {
+
+    }
+}
---
+++ project/Assets/Editor/ObjectStorage/Settings.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace UnityStaticData
+{
+    /// <summary>
+    /// Класс хранящий и управляющий настройками
+    /// </summary>
+    [Serializable]
+    public class Settings
+    {
+        private const string DEFAULT_PATH = "Assets/DataLayer/";
+        private const string SETTING_PATH = "Assets/Editor/UnityStaticData/s.set";
+        /// <summary>
+        /// Экземпляр настроек
+        /// </summary>
+        public static Settings Instance { get; private set; }
+        static Settings()
+        {
+            Instance = loadSettings();
+            if (string.IsNullOrEmpty(Instance.PathToSaveData))
+            {
+                Instance.PathToSaveData = DEFAULT_PATH;
+            }
+        }
+        /// <summary>
+        /// Получение валидного пути, для сохранения в настроенной папке проэкта
+        /// </summary>
+        /// <param name="fileName">Имя файла для сохранения</param>
+        /// <returns></returns>
+        public static string GetPathToSaveData(string fileName)
+        {
+            return Instance.PathToSaveData + fileName;
+        }
+        /// <summary>
+        /// Сохранение настроек
+        /// </summary>
+        public static void Save()
+        {
+            Serializator.SaveTo<Settings>(SETTING_PATH, Instance);
+        }
+        /// <summary>
+        /// Путь к папке сохранения данных
+        /// </summary>
+        public string PathToSaveData { get; set; }
+
+        private static Settings loadSettings()
+        {
+            // загружает их файла если настройки были сохранены на диске
+            try
+            {
+                return Serializator.LoadFrom<Settings>(SETTING_PATH);
+            }
+            catch (FileNotFoundException)
+            {
+                return new Settings();
+            }
+        }
+    }
+}
