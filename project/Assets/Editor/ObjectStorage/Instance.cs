f0d573d63c6d38f0ca512bd79ca645ec95a37e28
k1moshka <364844@gmail.com>
2014-10-19 19:24:25 +0700

    Добавление класса прдставляющего экземпляр схемы данных

---
+++ project/Assets/Editor/ObjectStorage/Instance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace UnityStaticData
+{
+    /// <summary>
+    /// Представление экземпляра сущности для схемы данных, необходимый для генерации репозитория или ресурсов
+    /// </summary>
+    public class Instance
+    {
+        /// <summary>
+        /// Схема для экземпляра
+        /// </summary>
+        public DataScheme DataScheme { get; set; }
+        /// <summary>
+        /// Значение для полей экземпляра
+        /// </summary>
+        public Dictionary<string, object> FieldsValues { get; set; }
+    }
+}
