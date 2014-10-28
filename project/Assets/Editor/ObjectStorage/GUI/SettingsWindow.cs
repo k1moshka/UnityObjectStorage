08b161bb3df5b2ac557191653956404682ab4d2c
k1moshka <364844@gmail.com>
2014-10-28 21:20:55 +0600

    Дописание окна настроек согласно классу настроек

--- project/Assets/Editor/ObjectStorage/GUI/SettingsWindow.cs
+++ project/Assets/Editor/ObjectStorage/GUI/SettingsWindow.cs
@@ -8,7 +8,11 @@ using UnityStaticData;
 public class SettingsWindow : EditorWindow
 {
     private Vector2 scrollPos;
-
+    private string renderMethodName;
+    private string renderMethodType;
+    private string typeName;
+    private string[] allTypes = Settings.GetRegisteredTypes();
+    
     public void OnGUI()
     {
         scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
@@ -16,6 +20,24 @@ public class SettingsWindow : EditorWindow
 
         Settings.Instance.PathToSaveData = EditorGUILayout.TextField("Path to save plugin data", Settings.Instance.PathToSaveData);
         Settings.Instance.PathToSaveSources = EditorGUILayout.TextField("Path to save generated sources", Settings.Instance.PathToSaveSources);
+
+        GUILayout.Label("Registered types:");
+
+        foreach (var t in allTypes)
+        {
+            GUILayout.TextField(t);
+        }
+        EditorGUILayout.Separator();
+        EditorGUILayout.Separator();
+
+        EditorGUILayout.LabelField("Add custom type");
+        typeName = EditorGUILayout.TextField("Full type name:", typeName);
+        renderMethodType = EditorGUILayout.TextField("Full type containing render method:", renderMethodType);
+        renderMethodName = EditorGUILayout.TextField("Render method name:", renderMethodName);
+        if (GUILayout.Button("Register new type..."))
+            RegisterCustomType();
+
+
         if (GUILayout.Button("Save")) Save();
 
         EditorGUILayout.EndVertical();
@@ -29,6 +51,16 @@ public class SettingsWindow : EditorWindow
     }
 
     #region API
+    public void RegisterCustomType()
+    {
+        Settings.RegisterNewType(new TypeDescriptor() { TypeName = typeName, RenderMethodType = renderMethodType, RenderMethodName = renderMethodName });
+
+        typeName = "";
+        renderMethodType = "";
+        renderMethodName = "";
+        allTypes = Settings.GetRegisteredTypes();
+    }
+
     public void Save()
     {
         Settings.Save();
