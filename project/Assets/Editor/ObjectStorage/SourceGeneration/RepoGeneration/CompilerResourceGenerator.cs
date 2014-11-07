3a0764af542a89b0c64a66fe05b83197b0af9b18
k1moshka <364844@gmail.com>
2014-11-07 21:30:01 +0600

    New: интерфейс генератора ресурсов

---
+++ project/Assets/Editor/ObjectStorage/SourceGeneration/RepoGeneration/CompilerResourceGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UnityStaticData
+{
+    /// <summary>
+    /// Генератор ресурсов основаный на компиляции сурсов во временную сборку и последующей сериализации
+    /// </summary>
+    public class CompilerResourceGenerator : IResourceGenerator
+    {
+        /// <summary>
+        /// Генерирование ресурсов на основе переданных схем данных
+        /// </summary>
+        /// <param name="path">Путь до файла в который сохраняться будут ресурсы</param>
+        /// <param name="schemes">Схемы инстансы которых необходимо сохранить в ресурсах</param>
+        public void GenerateResourceRepository(string path, DataScheme[] schemes)
+        {
+            
+        }
+    }
+}
---
+++ project/Assets/Editor/ObjectStorage/SourceGeneration/RepoGeneration/IResourceGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UnityStaticData
+{
+    /// <summary>
+    /// Интерфейс предоставляющий АПИ для генератора ресурсов проекта
+    /// </summary>
+    public interface IResourceGenerator
+    {
+        /// <summary>
+        /// Генерирование ресурсов на основе переданных схем данных
+        /// </summary>
+        /// <param name="path">Путь до файла в который сохраняться будут ресурсы</param>
+        /// <param name="schemes">Схемы инстансы которых необходимо сохранить в ресурсах</param>
+        void GenerateResourceRepository(string path, DataScheme[] schemes);
+    }
+}
