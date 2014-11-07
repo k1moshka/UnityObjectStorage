2124d3d56c1a70a99f43b0a86b3d9e49a9f4c17a
k1moshka <364844@gmail.com>
2014-11-07 22:56:53 +0600

    New: проект юнит тестов

---
+++ UnityObjectStorage.Tests/SourceGeneration/Resoures/CompilerResourceGeneratorTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnityStaticData;
+
+namespace UnityObjectStorage.Tests.SourceGeneration.Resoures
+{
+    [TestClass]
+    public class CompilerResourceGeneratorTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var compiler = new CompilerResourceGenerator();
+
+            //compiler.GenerateResourceRepository()
+        }
+    }
+}
